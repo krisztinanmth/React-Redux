449af97b85f43a5ad7e9b55fbc2c023d80caf748
Nemeth Krisztina <knemeth@b2international.com>
2019-05-09 16:36:04 +0200

    [components]: refactor and create reusable CommentDetail component

--- components/src/index.js
+++ components/src/index.js
@@ -1,59 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import faker from 'faker'
+import CommentDetails from './CommentDetail'
 
 const App = () => {
 
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date">today @6PM</span>
-          </div>
-          <div className="text">
-            nice blog post!
-          </div>
-        </div>
-      </div>
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date">today @6PM</span>
-          </div>
-          <div className="text">
-            nice blog post!
-          </div>
-        </div>
-      </div>
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date">today @6PM</span>
-          </div>
-          <div className="text">
-            nice blog post!
-          </div>
-        </div>
-      </div>
+      <CommentDetails />
+      <CommentDetails />
+      <CommentDetails />
     </div>
   )
 }
