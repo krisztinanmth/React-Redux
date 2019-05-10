b5eed7ba542e434d8d51c2d1f9858b5a36596324
Nemeth Krisztina <knemeth@b2international.com>
2019-05-10 17:04:00 +0200

    [seasons]: use geoLocation API of browser to get currentPosition of the user

---
+++ seasons/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  window.navigator.geolocation.getCurrentPosition(
+    position => console.log(position),
+    error => console.log(error)
+  );
+
+  return <div>hello</div>;
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
