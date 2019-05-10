b8cd447ebe9c372a68d91cf9316b1060feab6389
Nemeth Krisztina <knemeth@b2international.com>
2019-05-10 10:42:00 +0200

    [components]: pass all data from parent to child as props

--- components/src/CommentDetail.js
+++ components/src/CommentDetail.js
@@ -1,19 +1,19 @@
 import React from 'react'
-import faker from 'faker'
+// import faker from 'faker'
 
 const CommentDetail = props => {
 
   return (
     <div className="comment">
       <a href="/" className="avatar">
-        <img alt="avatar" src={faker.image.avatar()} />
+        <img alt="avatar" src={props.avatar} />
       </a>
       <div className="content">
         <a href="/" className="author">
           {props.author}
         </a>
         <div className="metadata">
-          <span className="date">today @6PM</span>
+          <span className="date">{props.timeOfPosting}</span>
         </div>
         <div className="text">
           {props.text}
--- components/src/index.js
+++ components/src/index.js
@@ -1,21 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import CommentDetails from './CommentDetail'
+import faker from 'faker'
 
 const App = () => {
 
   return (
     <div className="ui container comments">
       <CommentDetails
+        avatar={faker.image.avatar()}
         author="Alex"
+        timeOfPosting = "Yesterday 11:00 AM"
         text="Nice blog post!"
       />
       <CommentDetails
+        avatar={faker.image.avatar()}
         author="Sam"
+        timeOfPosting = "Today 2:00 AM"
         text="loved reading this one"
       />
       <CommentDetails
+        avatar={faker.image.avatar()}
         author="Julia"
+        timeOfPosting = "Today 5:00 PM"
         text="true words they are"
       />
     </div>
