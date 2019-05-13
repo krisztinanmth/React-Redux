c3f65e9ed6cce554c8413e01648d0f24474b3185
Nemeth Krisztina <knemeth@b2international.com>
2019-05-13 17:34:52 +0200

    [pics]: refactor App into class based component

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 
-const App = () => {
-  return (
-    <div className="ui container" style={{ marginTop: '2em' }}>
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: '2em' }}>
       <SearchBar />
     </div>
-  );
-};
+    );
+  };
+}
 
 export default App;
