af97922f327f2e33ebfa888bbbc55c4c9c2489a8
Nemeth Krisztina <knemeth@b2international.com>
2019-05-13 16:51:45 +0200

    [pics]: create onChange event handler for input in SearchBar

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  onInputChange(event) {
+    console.log(event.target.value);
+  }
+
   render() {
     return (
-      <div>
-        <form>
-          <input type="text" />
+      <div className="ui segment">
+        <form className="ui form">
+          <div className="field">
+          <label>image search</label>
+          <input type="text" onChange={this.onInputChange} />
+          </div>
         </form>
       </div>
     );
