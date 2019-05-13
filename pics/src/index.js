9c3e2dae5c6dc9cdfca48deff67cf8edf8d32c04
Nemeth Krisztina <knemeth@b2international.com>
2019-05-13 16:17:44 +0200

    [pics]: init project

---
+++ pics/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+      hello
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
