05c2294ba539c5b2b10927b693563db8fc397dde
Nemeth Krisztina <knemeth@b2international.com>
2019-05-13 16:21:55 +0200

    [pics]: move App into components folder

--- pics/src/index.js
+++ pics/src/index.js
@@ -1,12 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const App = () => {
-  return (
-    <div>
-      hello
-    </div>
-  );
-};
+import App from './components/App';
 
 ReactDOM.render(<App />, document.querySelector("#root"));
