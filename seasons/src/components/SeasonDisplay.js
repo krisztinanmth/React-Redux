de690dc4193cc419c981d8ffc68f5010b5e75ad1
Nemeth Krisztina <knemeth@b2international.com>
2019-05-13 13:10:39 +0200

    [seasons]: implement getSeason() functionality

--- seasons/src/components/SeasonDisplay.js
+++ seasons/src/components/SeasonDisplay.js
@@ -1,7 +1,17 @@
 import React from "react";
 
+const getSeason = (latitude, month) => {
+  if (month > 2 && month < 9) {
+    return latitude > 0 ? 'summer' : 'winter';
+  } else {
+    return latitude > 0 ? 'winter' : 'summer';
+  }
+}
+
 const SeasonDisplay = props => {
-  return <div>SeasonDisplay {props.lat}</div>;
+  const season = getSeason(props.lat, new Date().getMonth());
+
+  return <div>{season === 'summer' ? 'Let\'s have some ice-cream' : 'Brrrr, it\'s chilly'}</div>;
 };
 
 export default SeasonDisplay;
