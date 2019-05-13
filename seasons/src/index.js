c9ca5e8f44739429074f489e7b333e0a2e11e781
Nemeth Krisztina <knemeth@b2international.com>
2019-05-13 11:04:15 +0200

    [seasons]: introduce state in App component

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { latitude: null }
+  }
 
   render() {
-
     window.navigator.geolocation.getCurrentPosition(
       position => console.log(position),
       error => console.log(error)
