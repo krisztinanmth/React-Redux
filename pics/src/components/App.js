8627ebd0be30033d3cd226e148dd9dd4e9717a1b
Nemeth Krisztina <knemeth@b2international.com>
2019-05-14 09:24:53 +0200

    [pics]: implement onSearchSubmit functionality with axios GET request

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import axios from 'axios'
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
 
   onSearchSubmit(term) {
-    console.log(term)
+    axios.get('https://api.unsplash.com/search/photos/', {
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID d4d8262c47566d0f2a04583e3c4d25b561c57d83153872bff79561f3f563a1d1'
+      }
+    })
   }
 
   render() {
