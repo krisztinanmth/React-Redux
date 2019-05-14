ee886d7da12b2e12ef77383ef1031b45ad79bdb7
Nemeth Krisztina <knemeth@b2international.com>
2019-05-14 12:14:17 +0200

    [videos]: create SearchBar component

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,8 +1,13 @@
 import React from "react";
+import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   render() {
-    return <div>app</div>;
+    return (
+      <div className="ui container" style={{ marginTop: '1em' }} >
+        <SearchBar />
+      </div>
+    );
   }
 }
 
