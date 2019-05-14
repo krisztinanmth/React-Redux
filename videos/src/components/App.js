ddfd60c151d2c755836636db59bd597faa8cb769
Nemeth Krisztina <knemeth@b2international.com>
2019-05-14 20:30:34 +0200

    [videos]: add VideoDetail component

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 class App extends React.Component {
   state = { videos: [], selectedVideo: null };
@@ -23,10 +24,19 @@ class App extends React.Component {
     return (
       <div className="ui container" style={{ marginTop: '1em' }}>
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoList
-          onVideoSelect={this.onVideoSelect}
-          videos={this.state.videos}
-        />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- videos/src/components/VideoItem.js
+++ videos/src/components/VideoItem.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './VideoItem.css';
 
 const VideoItem = ({ video, onVideoSelect }) => {
-  const { description, thumbnails, title } = video.snippet;
+  const { thumbnails, title } = video.snippet;
 
   return (
     <div onClick={() => onVideoSelect(video)} className="video-item item">
-      <img className="ui image" alt={description} src={thumbnails.medium.url} />
+      <img className="ui image" alt={title} src={thumbnails.medium.url} />
       <div className="content">
         <div className="header">
           {title}
