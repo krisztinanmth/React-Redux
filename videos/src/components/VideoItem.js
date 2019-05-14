eb49e2efa155cb970c92c4d79f49d04a8a9ba016
Nemeth Krisztina <knemeth@b2international.com>
2019-05-14 16:04:43 +0200

    [videos]: add basic styling to VideoItem component

--- videos/src/components/VideoItem.js
+++ videos/src/components/VideoItem.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import './VideoItem.css';
 
 const VideoItem = ({ video }) => {
   const { description, thumbnails, title } = video.snippet;
 
   return (
-    <div>
-      <img alt={description} src={thumbnails.medium.url} />
-      {title}
+    <div className="video-item item">
+      <img className="ui image" alt={description} src={thumbnails.medium.url} />
+      <div className="content">
+        <div className="header">
+          {title}
+        </div>
+        <div className="description">
+
+        </div>
+      </div>
     </div>
   );
 };
