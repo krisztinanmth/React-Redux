82db646e2992559ffbdaa4d1d3bfee01b1c288a2
Nemeth Krisztina <knemeth@b2international.com>
2019-05-14 14:21:36 +0200

    [videos]: turn input in SearchBar into controlled component

--- videos/src/components/SearchBar.js
+++ videos/src/components/SearchBar.js
@@ -1,12 +1,29 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  state = { term: "" };
+
+  onInputChange = event => {
+    this.setState({ term: event.target.value })
+  };
+
+  onFormSubmit = event => {
+    event.preventDefault();
+  };
+
   render() {
+    console.log(this.state.term);
+
     return (
-      <div className="ui segment">
-        <form className="ui form">
+      <div className="search-bar ui segment">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
           <div className="field">
-            <input type="text" />
+            <label>video search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
           </div>
         </form>
       </div>
