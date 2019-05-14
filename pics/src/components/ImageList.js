6f1cd09d08d7b0eba1b4df603993f6818f4681ab
Nemeth Krisztina <knemeth@b2international.com>
2019-05-14 10:33:01 +0200

    [pics]: create CSS grid

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,12 +1,13 @@
 import React from "react";
+import './ImageList.css'
 
 const ImageList = props => {
 
   // destructure image as i need image.id, image.description & image.urls
   const images = props.images.map(({ description, id, urls }) => {
-    return <img className="ui medium bordered image" key={id} alt={description} src={urls.regular} />
+    return <img className="ui large bordered image" key={id} alt={description} src={urls.regular} />
   })
-  return <div>{images}</div>;
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
