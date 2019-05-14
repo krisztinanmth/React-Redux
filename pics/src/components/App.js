54621f8d46c9cb2c5fc34f5545e9cc98e33fc6ba
Nemeth Krisztina <knemeth@b2international.com>
2019-05-14 09:44:43 +0200

    [pics]: refactor onSearchSubmit into arrow function so this keyword doesn't throw error

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -3,22 +3,23 @@ import axios from 'axios'
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
+  state = { images: [] }
 
-  async onSearchSubmit(term) {
+  onSearchSubmit = async term => {
     const response = await axios.get('https://api.unsplash.com/search/photos/', {
       params: { query: term },
       headers: {
         Authorization: 'Client-ID d4d8262c47566d0f2a04583e3c4d25b561c57d83153872bff79561f3f563a1d1'
       }
     });
-
-    console.log(response.data.results)
+    this.setState({ images: response.data.results })
   }
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: '2em' }}>
       <SearchBar onSubmit={this.onSearchSubmit} />
+      found {this.state.images.length} images
     </div>
     );
   }
