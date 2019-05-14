049fad67669d62bcf4a9580cb8307fd739172cda
Nemeth Krisztina <knemeth@b2international.com>
2019-05-14 20:00:08 +0200

    [videos]: implement onVideoSelect functionality - pass it down as props to VideoItem

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -4,7 +4,7 @@ import youtube from '../apis/youtube';
 import VideoList from './VideoList';
 
 class App extends React.Component {
-  state = { videos: [] };
+  state = { videos: [], selectedVideo: null };
 
   onTermSubmit = async term => {
     const response = await youtube.get('/search', {
@@ -15,11 +15,18 @@ class App extends React.Component {
     this.setState({ videos: response.data.items });
   };
 
+  onVideoSelect = video => {
+    console.log('from the app:', video);
+  };
+
   render() {
     return (
       <div className="ui container" style={{ marginTop: '1em' }}>
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoList videos={this.state.videos} />
+        <VideoList
+          onVideoSelect={this.onVideoSelect}
+          videos={this.state.videos}
+        />
       </div>
     );
   }
--- videos/src/components/VideoItem.js
+++ videos/src/components/VideoItem.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import './VideoItem.css';
 
-const VideoItem = ({ video }) => {
+const VideoItem = ({ video, onVideoSelect }) => {
   const { description, thumbnails, title } = video.snippet;
 
   return (
-    <div className="video-item item">
+    <div onClick={() => onVideoSelect(video)} className="video-item item">
       <img className="ui image" alt={description} src={thumbnails.medium.url} />
       <div className="content">
         <div className="header">
           {title}
-        </div>
-        <div className="description">
-
         </div>
       </div>
     </div>
