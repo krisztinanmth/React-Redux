29518474e936abab9979834b7aae0c8edd0eb47e
Nemeth Krisztina <knemeth@b2international.com>
2019-05-16 16:56:07 +0200

    [blog]: render list of posts after fetching

--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -8,12 +8,28 @@ class PostList extends React.Component {
     this.props.fetchPosts()
   }
 
+  renderList = () => {
+    return this.props.posts.map(post => {
+      return (
+        <div key={post.id} className="item">
+          <i className="large middle aligned icon user"/>
+          <div className="content">
+            <div className="description">
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        </div>
+      )
+    })
+  }
+
   render() {
     console.log(this.props.posts)
 
     return (
-      <div>
-        hello post list
+      <div className="ui relaxed divided list">
+        {this.renderList()}
       </div>
     )
   }
