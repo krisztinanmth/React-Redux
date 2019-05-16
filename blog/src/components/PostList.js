92e73576ecd421bb87e87a3b9dcc7a7c39d04735
Nemeth Krisztina <knemeth@b2international.com>
2019-05-16 13:56:08 +0200

    [blog]: implement postsReducer functionality

--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -6,7 +6,6 @@ class PostList extends React.Component {
 
   componentDidMount() {
     this.props.fetchPosts()
-    console.log(this.props.fetchPosts().type)
   }
 
   render() {
--- blog/src/reducers/index.js
+++ blog/src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux'
+import postsReducer from './postsReducer'
 
 export default combineReducers({
-  replaceMe: () => 'replace this dummy reducer with a real one'
+  posts: postsReducer
 })
