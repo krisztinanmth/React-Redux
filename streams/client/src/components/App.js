a127fbdf49e58a346acce81ff6313e252a8361e4
Nemeth Krisztina <knemeth@b2international.com>
2019-05-17 17:41:05 +0200

    [stream/client]: create Header component

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { BrowserRouter, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
 
+import Header from './Header'
 import StreamCreate from './streams/StreamCreate'
 import StreamDelete from './streams/StreamDelete'
 import StreamEdit from './streams/StreamEdit'
@@ -12,6 +13,7 @@ const App = () => {
     <div className="ui container">
       <BrowserRouter>
         <div>
+          <Header />
           <Route path='/' exact component={StreamList} />
           <Route path='/streams/new' exact component={StreamCreate} />
           <Route path='/streams/edit' exact component={StreamEdit} />
