785606e5ec890a61e2818c918b5052e1604859e9
Nemeth Krisztina <knemeth@b2international.com>
2019-05-20 10:27:37 +0200

    [streams/client]: connect GoogleAuth component with action creators

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { signIn, signOut } from '../actions'
 
 class GoogleAuth extends React.Component {
   state = { isSignedIn: null }
@@ -10,14 +12,19 @@ class GoogleAuth extends React.Component {
         scope: 'email'
       }).then(() => {
         this.auth = window.gapi.auth2.getAuthInstance()
+        // this.setState({ isSignedIn: this.auth.isSignedIn.get() })
         this.onAuthChange()
         this.auth.isSignedIn.listen(this.onAuthChange)
       })
     })
   }
 
-  onAuthChange = () => {
-    this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+  onAuthChange = (isSignedIn) => {
+    if (isSignedIn) {
+      this.props.signIn()
+    } else {
+      this.props.signOut()
+    }
   }
 
   onSignInClick = () => {
@@ -57,4 +64,7 @@ class GoogleAuth extends React.Component {
   }
 }
 
-export default GoogleAuth
+export default connect(
+  null,
+  { signIn, signOut }
+)(GoogleAuth)
