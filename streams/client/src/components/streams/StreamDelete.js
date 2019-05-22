a230db244090bdbdeb3cca0b542a1d4a973eae26
Nemeth Krisztina <knemeth@b2international.com>
2019-05-22 17:55:17 +0200

    [streams/client]: create Portal

--- streams/client/src/components/streams/StreamDelete.js
+++ streams/client/src/components/streams/StreamDelete.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import Modal from '../Modal'
 
 const StreamDelete = () => {
-  return <div>Delete Stream</div>
+  return (
+    <div>
+      Delete Stream
+      <Modal />
+    </div>
+  )
 }
 
 export default StreamDelete
