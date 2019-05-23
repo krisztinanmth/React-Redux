cf9ac26c8527c1b68bc9085b216519c1d3c15d8d
Nemeth Krisztina <knemeth@b2international.com>
2019-05-23 11:52:44 +0200

    [streams/client]: stop onClick propagation

--- streams/client/src/components/Modal.js
+++ streams/client/src/components/Modal.js
@@ -8,12 +8,15 @@ const Modal = props => {
       onClick={() => history.push('/')}
       className="ui dimmer modals visible active"
     >
-      <div className="ui standard modal visible active">
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className="ui standard modal visible active"
+      >
         <div className="header">delete stream</div>
         <div className="content">are you sure you want to delete this stream?</div>
         <div className="actions">
-          <button className="ui button negative">delete</button>
-          <button className="ui button secondary">cancel</button>
+          <button className="ui button secondary">delete</button>
+          <button className="ui button">cancel</button>
         </div>
       </div>
     </div>,
