4b3ad02b1c54deefa06b0567122dcbf6cf4078db
Nemeth Krisztina <knemeth@b2international.com>
2019-05-23 11:35:12 +0200

    [streams/client]: implement onClick functionality in Modal

--- streams/client/src/components/Modal.js
+++ streams/client/src/components/Modal.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import history from '../history'
 
 const Modal = props => {
   return ReactDOM.createPortal(
-    <div className="ui dimmer modals visible active">
+    <div
+      onClick={() => history.push('/')}
+      className="ui dimmer modals visible active"
+    >
       <div className="ui standard modal visible active">
-        MODAL
+        <div className="header">delete stream</div>
+        <div className="content">are you sure you want to delete this stream?</div>
+        <div className="actions">
+          <button className="ui button negative">delete</button>
+          <button className="ui button secondary">cancel</button>
+        </div>
       </div>
     </div>,
     document.querySelector('#modal')
