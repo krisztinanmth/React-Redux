9769f85dd91e71cec453228c73c3cf37d6076f28
Nemeth Krisztina <knemeth@b2international.com>
2019-05-24 11:59:58 +0200

    [translate]: setState({ language })- according to which flag is clicked

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -1,9 +1,30 @@
 import React from 'react';
 
-const App = () => {
-  return (
-    <div className="ui container">App</div>
-  )
-};
+class App extends React.Component {
+  state = { language: 'english' };
+
+  onLanguageChange = language => {
+    this.setState({ language })
+  }
+
+  render() {
+    return (
+      <div className="ui container">
+        <div>
+          select a language:
+          <i
+            className="flag gb"
+            onClick={() => this.onLanguageChange('english')}
+          />
+          <i
+            className="flag nl"
+            onClick={() => this.onLanguageChange('dutch')}
+          />
+        </div>
+        {this.state.language}
+      </div>
+    )
+  }
+}
 
 export default App;
