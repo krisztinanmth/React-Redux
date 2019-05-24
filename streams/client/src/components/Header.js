fdac7f1bc096d55fe3d727a43e2515a98a1564f6
Nemeth Krisztina <knemeth@b2international.com>
2019-05-24 11:07:26 +0200

    [streams/client]: create FLV player

--- streams/client/src/components/Header.js
+++ streams/client/src/components/Header.js
@@ -7,11 +7,11 @@ const Header = () => {
   return (
     <div className="ui secondary pointing menu">
       <Link to='/' className="item">
-        streamer
+        all streams
       </Link>
       <div className="right menu">
         <Link to='/' className="item">
-          all streams
+          streamy
         </Link>
         <GoogleAuth />
       </div>
