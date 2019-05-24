f81a5ec3f47e72dbfd965855f54f91c18ad229e4
Nemeth Krisztina <knemeth@b2international.com>
2019-05-24 16:45:23 +0200

    [translate]: connect Context Object to Button component

--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
 
 class Button extends React.Component {
+  // set up context type -> this is how I connect
+  // my Context Object with this class
+  // NOTE: MUST BE CALLED contextTypea and MUST BE static
+  static contextType = LanguageContext;
+
   render() {
+    console.log(this.context)
     return (
       <div>
         <button className="ui button orange">button</button>
@@ -9,5 +16,8 @@ class Button extends React.Component {
     )
   }
 }
+
+
+// Button.contextType = LanguageContext; -> would be good also for setting up contextType
 
 export default Button;
