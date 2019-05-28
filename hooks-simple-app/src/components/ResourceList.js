cf91695a2e79b89bae1f77dd5cd5bcda690321fb
Nemeth Krisztina <knemeth@b2international.com>
2019-05-28 11:57:23 +0200

    [hooks-simple-app]: extract hooks related logic into its own seperate function

--- hooks-simple-app/src/components/ResourceList.js
+++ hooks-simple-app/src/components/ResourceList.js
@@ -1,16 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const ResourceList = ({ resource }) => {
+const useResources = resource => {
   const [resources, setResources] = useState([]);
 
-  useEffect(() => {
-    (async resource => {
-      const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
+  useEffect(
+    () => {
+      (async resource => {
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
 
-      setResources(response.data)
-    })(resource);
-  }, [resource])
+        setResources(response.data);
+      })(resource);
+    },
+    [resource]
+  );
+  return resources;
+};
+
+const ResourceList = ({ resource }) => {
+  const resources = useResources(resource);
 
   return (
     <ul>
